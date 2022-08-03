6a8f1a2234be6ef4f116ffc755d0620647292857
Chaitanya <thakur.chaitanya25@gmail.com>
2022-08-03 14:05:39 +0530

    update seeds.py

--- backend/app/db/seeds.py
+++ backend/app/db/seeds.py
@@ -1,1 +1,19 @@
-print('Please fill the seeds file')
+import psycopg2
+
+conn = psycopg2.connect(
+    database="anythink-market",
+    user='postgres',
+    password='postgres',
+    host='postgres',
+    port='5432'
+)
+
+cursor = conn.cursor()
+
+for i in range(100):
+    cursor.execute("""INSERT INTO users(username, email, password) VALUES("user{i}", "user{i}@mail.com", "passwd{i}")""")
+    cursor.execute("""INSERT INTO items(slug, title, description) VALUES("item{i}", "title{i}", "description for item {i}")""")
+    cursor.execute("""INSERT INTO comments(body, item_id) VALUES("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", {i})""")
+
+cursor.close()
+conn.close()
