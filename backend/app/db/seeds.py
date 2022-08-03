aa11a594fe66a7bcada2568d887ef5fca22035e4
Chaitanya <thakur.chaitanya25@gmail.com>
2022-08-03 22:09:15 +0530

    fixed merge conflict

--- backend/app/db/seeds.py
+++ backend/app/db/seeds.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 from sqlalchemy import create_engine
 
@@ -11,6 +10,3 @@ with engine.connect() as con:
         con.execute("""INSERT INTO users(username, email, hashed_password) VALUES("user{i}", "user{i}@mail.com", "passwd{i}")""")
         con.execute("""INSERT INTO items(slug, title, description, seller_id) VALUES("item{i}", "title{i}", "description for item {i}", {i}{i})""")
         con.execute("""INSERT INTO comments(body, seller_id, item_id) VALUES("comment{i}", {i}{i}, {i})""")
-=======
-print('Please fill the seeds file')
->>>>>>> refs/remotes/origin/main
