77b502f18ebeb71e9d98dd8b93c3a62b161c978b
Chaitanya <thakur.chaitanya25@gmail.com>
2022-08-06 20:16:47 +0530

    fix lint error

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -1,16 +1,18 @@
-import React from "react";
-import agent from '../../agent'
+import React from "react"
+import agent from '../../agent';
 
 const SearchBox = () => {
     return (
         <div>
-            <input id="search-box" placeholder="What is it that you truly desire?"
-            onInput={(ev) => {
-              const value = ev.target.value;
-              if (value.length >= 3) agent.Items.byTitle(value);
-          }} />
+            <input
+                id="search-box"
+                placeholder="What is it that you truly desire?"
+                onInput={(ev) => {
+                    const value = ev.target.value;
+                    if (value.length >= 3) agent.Items.byTitle(value);
+                }} />
         </div>
-    )
+    )    
 }
 
 export default SearchBox;
